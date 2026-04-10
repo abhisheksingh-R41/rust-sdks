2ba470a7d987763a0f1b9e5e6f5a51682d43f33d
Ryan Gaus <rsg1egoman@gmail.com>
2026-04-10 17:04:50 -0400

    feat: implement client protocol support in rust sdk

--- livekit/src/room/participant/local_participant.rs
+++ livekit/src/room/participant/local_participant.rs
@@ -121,6 +121,7 @@ impl LocalParticipant {
         joined_at: i64,
         encryption_type: EncryptionType,
         permission: Option<proto::ParticipantPermission>,
+        client_protocol: i32,
     ) -> Self {
         Self {
             inner: super::new_inner(
@@ -135,6 +136,7 @@ impl LocalParticipant {
                 kind_details,
                 joined_at,
                 permission,
+                client_protocol,
             ),
             local: Arc::new(LocalInfo {
                 events: LocalEvents::default(),
@@ -828,6 +830,10 @@ impl LocalParticipant {
 
     pub fn permission(&self) -> Option<proto::ParticipantPermission> {
         self.inner.info.read().permission.clone()
+    }
+
+    pub fn client_protocol(&self) -> i32 {
+        self.inner.info.read().client_protocol
     }
 
     pub async fn perform_rpc(&self, data: PerformRpcData) -> Result<String, RpcError> {
